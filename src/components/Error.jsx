5488f64ed8eac8786914c1039e28caaae772a8fe
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2025-03-24 14:42:01 +0000

    feat: implement error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,4 +1,20 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+import { ButtonPrimary } from "./Buttons"
+
+const Wrapper = styled.div`
+  min-height: 100%;
+  display: grid;
+  place-content: center;
+`
 function Error() {
-  return <div>Error</div>
+  return (
+    <Wrapper>
+      <p>404 | Not Found</p>
+      <ButtonPrimary as={Link} to="/" viewTransition>
+        Home
+      </ButtonPrimary>
+    </Wrapper>
+  )
 }
 export default Error
