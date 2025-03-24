d1e21806ec5259ab776b77fa50fe054360e3266d
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2025-03-24 14:39:21 +0000

    feat: implement home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,31 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+import { ButtonPrimary, ButtonSecondary } from "./Buttons"
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  padding: 3rem 1.2rem;
+`
+
 function Home() {
-  return <div>Home</div>
+  return (
+    <Wrapper>
+      <h1>Welcome to PopX</h1>
+      <p>
+        Lorem ipsum dolor sit amet <br />
+        consectetur adipisicing elit.
+      </p>
+
+      <ButtonPrimary as={Link} to="/signup" viewTransition>
+        Create Account
+      </ButtonPrimary>
+      <ButtonSecondary as={Link} to="/login" viewTransition>
+        Already Registered? Login
+      </ButtonSecondary>
+    </Wrapper>
+  )
 }
 export default Home
